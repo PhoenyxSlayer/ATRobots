a92b703994db82320a2d67d185252ed6f9b24323
Daniel Pellegrino <danpellegrino@github.com>
2022-09-12 12:39:19 -0400

    init commit
    Working GUI using JPanels, edited window class and main.

---
+++ ATRobots/src/com/redteam/atrobots/ATRobots.java
@@ -0,0 +1,33 @@
+package com.redteam.atrobots;
+
+import com.redteam.atrobots.screens.*;
+
+public class ATRobots {
+	
+	private Window gameWindow;
+	
+	// Game States
+	public enum State
+	{
+	    START, RUNNING, SETTINGS, CLOSING;
+	}
+	
+	// Resolution
+	public static int getWidth() { return 1280; }
+	public static int getHeight() { return 720; }
+	
+	// Main function
+	public static void main(String[] args) {
+		new ATRobots();
+	}
+	
+	// Add All Game States
+	public ATRobots() {
+		gameWindow = new Window("ATRobots", getWidth(), getHeight(), this);
+		gameWindow.addScreen(State.START, new StartScreen());
+		gameWindow.addScreen(State.RUNNING, new RunningScreen());
+		gameWindow.addScreen(State.SETTINGS, new SettingsScreen());
+		gameWindow.addScreen(State.CLOSING, new ClosingScreen());
+		gameWindow.run();
+	}
+}
--- ATRobots/src/com/redteam/atrobots/Window.java
+++ ATRobots/src/com/redteam/atrobots/Window.java
@@ -1,70 +1,54 @@
 package com.redteam.atrobots;
 
-import java.awt.Canvas;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.image.BufferStrategy;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.JFrame;
+import com.redteam.atrobots.ATRobots.*;
+import com.redteam.atrobots.screens.Screen;
 
 public class Window extends JFrame{
+	
 	private static final long serialVersionUID = 6286634599679800388L;
 	
-	private int width, height;
-	private Canvas canvas;
+	// Begin state with Start Screen
+	private State state = State.START;
 	
-	private Main main;
+	// Hash maps for the Screen States
 	
-	private Graphics g;
-	private BufferStrategy bs;
+	// SOURCE: https://stackoverflow.com/questions/34560139/how-can-i-create-a-start-screen-for-my-java-game
+	private Map<State, Screen> screens = new HashMap<>();
 	
-	public Window(String title, int width, int height, Main main) {
+	// JFrame Window Creation
+	public Window(String title, int width, int height, ATRobots main) {
 		super (title);
-		this.main = main;
-		
-		this.width = width;
-		this.height = height;
-		
+		// JFrame Formatting
 		setSize(width, height);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 		setResizable(false);
 		setLocationRelativeTo(null);
-		
-		canvas = new Canvas();
-		canvas.setSize(width, height);
-		canvas.setPreferredSize(new Dimension(width, height));
-	    canvas.setMinimumSize(new Dimension(width, height));
-	    canvas.setMaximumSize(new Dimension(width, height));
-	    canvas.setBackground(Color.BLACK);
-	    add(canvas);
-	    pack();
-	    
-	    init();
-	    run();
-	}
-	private void run() {
-		while(true) {
-			main.tick();
-			render();
-		}
 	}
 	
-	private void init() {
-		main.init();
+	// Adds Screens Classes w/ what state their associated w/ to HashMap
+	public void addScreen(State state, Screen screen) {
+        screens.put(state, screen);
+    }
+	
+	// Use this function to change the game state
+	public void updateState(State newState) {
+		revalidate();
+   	 	repaint();
+		state = newState;
+		run();
 	}
-	private void render() {
-		if(bs == null) canvas.createBufferStrategy(3);
-		
-		bs = canvas.getBufferStrategy();
-		g = bs.getDrawGraphics();
-		
-		g.clearRect(-200, -200, width+200, height+200);
-		
-		main.render(g);
-		
-		bs.show();
-		g.dispose();
+	
+	// Grabs current state and runs through the three functions consistently
+	public void run() {
+		Screen screen = screens.get(state);
+		screen.grabWindow(this);
+		screen.handleInput();
+		screen.update();
+        screen.draw();
 	}
 }
