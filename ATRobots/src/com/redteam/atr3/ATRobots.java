d1ed09b9e5943bc1cf426d58898528d81bf2f660
Daniel Pellegrino <64383789+danpellegrino@users.noreply.github.com>
2022-09-28 20:16:58 -0400

    changed window functionality

--- ATRobots/src/com/redteam/atr3/ATRobots.java
+++ ATRobots/src/com/redteam/atr3/ATRobots.java
@@ -1,32 +1,25 @@
 package com.redteam.atr3;
 
-import com.redteam.atr3.screens.*;
-
 public class ATRobots {
-	
-	private Window gameWindow;
+	public static final int WIDTH = 1280, HEIGHT = 720, FPS = 60;
+	public static Window window = new Window(WIDTH, HEIGHT, FPS, "AT-Robots");
 	
 	// Game States
 	public enum State
 	{
 	    START, RUNNING;
 	}
-	
-	// Resolution
-	public static int getWidth() { return 1280; }
-	public static int getHeight() { return 720; }
-	
 	// Main function
 	public static void main(String[] args) {
-		new ATRobots();
-	}
-	
-	// Add All Game States
-	public ATRobots() {
-
-		Window gameWindow = new Window("Title", 1280, 720, this);
-		gameWindow.addScreen(State.START, new StartScreen());
-		gameWindow.addScreen(State.RUNNING, new RunningScreen());
-		gameWindow.run();
+		window.create();
+		
+		while(!window.closed()) {
+			if(window.isUpdating()) {
+				window.update();
+				
+				window.swapBuffers();
+			}
+		}
+		window.stop();
 	}
 }
--- ATRobots/src/com/redteam/atr3/Window.java
+++ ATRobots/src/com/redteam/atr3/Window.java
@@ -1,64 +1,130 @@
 package com.redteam.atr3;
 
-import java.util.HashMap;
-import java.util.Map;
-import static org.lwjgl.glfw.GLFW.*;
+import java.nio.DoubleBuffer;
 
-import javax.swing.JFrame;
-import com.redteam.atr3.ATRobots.*;
-import com.redteam.atr3.screens.Screen;
+import org.lwjgl.BufferUtils;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWVidMode;
 
-
-public class Window extends JFrame{
-	
-	private static final long serialVersionUID = 6286634599679800388L;
-	
-	// Begin state with Start Screen
-	private State state = State.RUNNING;
-	
-	// Hash maps for the Screen States
-	// SOURCE: https://stackoverflow.com/questions/34560139/how-can-i-create-a-start-screen-for-my-java-game
-	private Map<State, Screen> screens = new HashMap<>();
-	
-	// JFrame Window Creation
-	public Window(String title, int width, int height, ATRobots main) {
-		glfwInit();
-
-		long win = glfwCreateWindow(width, height, title, 0, 0);
-
-		glfwShowWindow(win);
-
-
-		while (glfwWindowShouldClose(win) != true) {
-			glfwWaitEvents();
-		}
-		// JFrame Formatting
-		/*setSize(width, height);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);
-		setResizable(false);
-		setLocationRelativeTo(null);*/
-	}
-	
-	// Adds Screens Classes w/ what state their associated w/ to HashMap
-	public void addScreen(State state, Screen screen) {
-        screens.put(state, screen);
+public class Window	
+{
+	private int width, height;
+    private String title;
+    private double fps_cap, time, processedTime = 0;
+    private long window;
+    private boolean[] keys = new boolean[GLFW.GLFW_KEY_LAST];
+    private boolean[] mouseButtons = new boolean[GLFW.GLFW_MOUSE_BUTTON_LAST];
+    
+    public Window(int width, int height, int fps, String title) {
+    	this.width = width;
+    	this.height = height;
+    	this.title = title;
+    	fps_cap = fps;
     }
-	
-	// Use this function to change the game state
-	public void updateState(State newState) {
-		revalidate();
-   	 	repaint();
-		state = newState;
-		run();
-	}
-	
-	// Grabs current state and runs through the three functions consistently
-	public void run() {
-		Screen screen = screens.get(state);
-		screen.grabWindow(this);
-		screen.handleInput();
-		screen.update();
-        screen.draw();
-	}
+    
+    public void create() {
+    	if (!GLFW.glfwInit()) {
+    		System.err.println("ERROR: COULD NOT INITIALIZE GLFW");
+    		System.exit(-1);
+    	}
+    	GLFW.glfwWindowHint(GLFW.GLFW_VISIBLE, GLFW.GLFW_FALSE);
+    	GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GLFW.GLFW_FALSE);
+    	
+    	window = GLFW.glfwCreateWindow(width, height, title, 0, 0);
+    	
+    	if (window == 0) {
+    		System.err.println("ERR: WINDOW COULD NOT BE CREATED");
+    		System.exit(-1);
+    	}
+    	
+    	GLFWVidMode videoMode = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor());
+    	
+    	// MIGHT BE WRONG
+    	GLFW.glfwSetWindowPos(window, (videoMode.width() - width) / 2, (videoMode.height() - height) / 2);
+    	
+    	GLFW.glfwShowWindow(window);
+    	
+    	time = getTime();
+    }
+
+    public boolean closed() {
+    	return GLFW.glfwWindowShouldClose(window);
+    }
+    
+    public void update() {
+    	for (int i = 0; i < GLFW.GLFW_KEY_LAST; i++) keys[i] = isKeyDown(i);
+    	for (int i = 0; i < GLFW.GLFW_MOUSE_BUTTON_LAST; i++) mouseButtons[i] = isMouseDown(i);
+    	GLFW.glfwPollEvents();
+    }
+    
+    public void stop() {
+    	GLFW.glfwTerminate();
+    }
+    
+    public void swapBuffers() {
+    	GLFW.glfwSwapBuffers(window);
+    }
+    
+    public double getTime() {
+    	return (double) System.nanoTime() / (double) 1000000000;
+    }
+    
+    public boolean isKeyDown(int keyCode) {
+    	return GLFW.glfwGetKey(window, keyCode) == 1;
+    }
+    
+    public boolean isMouseDown(int mouseButton) {
+    	return GLFW.glfwGetMouseButton(window, mouseButton) == 1;
+    }
+    
+    public boolean isKeyPressed(int keyCode) {
+    	return isKeyDown(keyCode) && !keys[keyCode];
+    }
+    
+    public boolean isKeyReleased(int keyCode) {
+    	return !isKeyDown(keyCode) && keys[keyCode];
+    }
+    
+    public boolean isMousePressed(int mouseButton) {
+    	return isMouseDown(mouseButton) && !mouseButtons[mouseButton];
+    }
+    
+    public boolean isMouseReleased(int mouseButton) {
+    	return !isMouseDown(mouseButton) && mouseButtons[mouseButton];
+    }
+    
+    public double getMouseX() {
+    	DoubleBuffer buffer = BufferUtils.createDoubleBuffer(1);
+    	GLFW.glfwGetCursorPos(window, buffer, null);
+    	return buffer.get(0);
+    }
+    
+    public double getMouseY() {
+    	DoubleBuffer buffer = BufferUtils.createDoubleBuffer(1);
+    	GLFW.glfwGetCursorPos(window, null, buffer);
+    	return buffer.get(0);
+    }
+    
+    public boolean isUpdating() {
+    	double nextTIme = getTime();
+    	double passedTime = nextTIme - time;
+    	processedTime += passedTime;
+    	time = nextTIme;
+    	
+    	while(processedTime > 1.0/fps_cap) {
+    		processedTime -= 1.0/fps_cap;
+    		return true;
+    	}
+    	return false;
+    }
+    
+    public int getWidth() { return width; }
+    
+    public int getHeight() { return height; }
+    
+    public String getTitle() { return title; }
+    
+    public double getFPS() { return fps_cap; }
+    
+    public long getWindow() { return window; }
 }
