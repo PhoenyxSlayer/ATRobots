0770403f1a85042d579aa89869806eb1ff5bd735
Ethan <sowletha@gmail.com>
2022-11-07 13:36:14 -0500

    updated TestTankEntity.java

--- ATRobots/src/test/java/com/redteam/engine/core/entity/TestTankEntity.java
+++ ATRobots/src/test/java/com/redteam/engine/core/entity/TestTankEntity.java
@@ -9,12 +9,16 @@ class TestTankEntity {
 	
 	Vector3f originalPos = new Vector3f(0,0,0);
 	Vector3f originalRotation = new Vector3f(0,0,0);
+	Vector3f ogBaseRotation = new Vector3f(0,0,0);
+	Vector3f ogTurretRotation = new Vector3f(0,0,0);
 	
 	TankEntity tank = new TankEntity("tank", originalPos, originalRotation);
 	
 	
 	Vector3f updatedPos = new Vector3f(50,20,10);
 	Vector3f updatedRotation = new Vector3f(10,1,23);
+	Vector3f updatedBaseRotate = new Vector3f(37, 145, 8);
+	Vector3f updatedTurretRotate = new Vector3f(101,3,134);
 	
 	
 	// TankEntity CONSTRUCTOR TEST
@@ -96,19 +100,70 @@ class TestTankEntity {
 	
 	@Test
 	void testSetRotation() {
-		fail("Not yet implemented");
+		tank.setRotation(originalRotation.x, originalRotation.y, originalRotation.z);
+		
+		assertEquals(0.0f, tank.getRotation().x, 0.01f);
+		assertEquals(0.0f, tank.getRotation().y, 0.01f);
+		assertEquals(0.0f, tank.getRotation().z, 0.01f);
+		
+		tank.setRotation(updatedRotation.x, updatedRotation.y, updatedRotation.z);
+		
+		assertEquals(10.0f, tank.getRotation().x, 0.01f);
+		assertEquals(1.0f, tank.getRotation().x, 0.01f);
+		assertEquals(23.0f, tank.getRotation().x, 0.01f);
+		
+		// SPECIAL TO SETROTATION
+		tank.setRotation(30.0f, 10.0f, 5.0f);
+		
+		assertEquals(30.0f, tank.getRotation().x, 0.01f);
+		assertEquals(10.0f, tank.getRotation().x, 0.01f);
+		assertEquals(5.0f, tank.getRotation().x, 0.01f);
 	}
 	
 	// BASE ROTATION TEST
 	
 	@Test
 	void testIncBaseRotation() {
-		fail("Not yet implemented");
+		tank.setBaseRotation(ogBaseRotation.x, ogBaseRotation.y, ogBaseRotation.z);
+		
+		assertEquals(0.0f, tank.getBaseRotation().x, 0.01f);
+		assertEquals(0.0f, tank.getBaseRotation().y, 0.01f);
+		assertEquals(0.0f, tank.getBaseRotation().z, 0.01f);
+		
+		tank.incBaseRotation(updatedBaseRotate.x, updatedBaseRotate.y, updatedBaseRotate.z);
+		
+		assertEquals(37.0f, tank.getBaseRotation().x, 0.01f);
+		assertEquals(145.0f, tank.getBaseRotation().y, 0.01f);
+		assertEquals(8.0f, tank.getBaseRotation().z, 0.01f);
+		
+		// SPECIAL TO INCBASEROTATION
+		tank.incBaseRotation(3.0f, 5.0f, 42.0f);
+		
+		assertEquals(40.0f, tank.getBaseRotation().x, 0.01f);
+		assertEquals(150.0f, tank.getBaseRotation().y, 0.01f);
+		assertEquals(50.0f, tank.getBaseRotation().z, 0.01f);
 	}
 	
 	@Test
 	void testSetBaseRotation() {
-		fail("Not yet implemented");
+		tank.setBaseRotation(ogBaseRotation.x, ogBaseRotation.y, ogBaseRotation.z);
+		
+		assertEquals(0.0f, tank.getBaseRotation().x, 0.01f);
+		assertEquals(0.0f, tank.getBaseRotation().y, 0.01f);
+		assertEquals(0.0f, tank.getBaseRotation().z, 0.01f);
+		
+		tank.setBaseRotation(updatedBaseRotate.x, updatedBaseRotate.y, updatedBaseRotate.z);
+		
+		assertEquals(37.0f, tank.getBaseRotation().x, 0.01f);
+		assertEquals(145.0f, tank.getBaseRotation().y, 0.01f);
+		assertEquals(8.0f, tank.getBaseRotation().z, 0.01f);
+		
+		// SPECIAL TO SETBASEROTATION
+		tank.setBaseRotation(12.0f, 104.0f, 65.0f);
+		
+		assertEquals(12.0f, tank.getBaseRotation().x, 0.01f);
+		assertEquals(104.0f, tank.getBaseRotation().y, 0.01f);
+		assertEquals(65.0f, tank.getBaseRotation().z, 0.01f);
 	}
 	
 	
@@ -116,11 +171,45 @@ class TestTankEntity {
 	
 	@Test
 	void testIncTurretRotation() {
-		fail("Not yet implemented");
+		tank.setTurretRotation(ogTurretRotation.x, ogTurretRotation.y, ogTurretRotation.z);
+		
+		assertEquals(0.0f, tank.getTurretRotation().x, 0.01f);
+		assertEquals(0.0f, tank.getTurretRotation().y, 0.01f);
+		assertEquals(0.0f, tank.getTurretRotation().z, 0.01f);
+		
+		tank.incTurretRotation(updatedTurretRotate.x, updatedTurretRotate.y, updatedTurretRotate.z);
+		
+		assertEquals(101.0f, tank.getTurretRotation().x, 0.01f);
+		assertEquals(3.0f, tank.getTurretRotation().y, 0.01f);
+		assertEquals(134.0f, tank.getTurretRotation().z, 0.01f);
+		
+		// SPECIAL TO INCTURRETROTATION
+		tank.incTurretRotation(9.0f, 7.0f, 6.0f);
+		
+		assertEquals(110.0f, tank.getTurretRotation().x, 0.01f);
+		assertEquals(10.0f, tank.getTurretRotation().y, 0.01f);
+		assertEquals(140.0f, tank.getTurretRotation().z, 0.01f);
 	}
 	
 	@Test
 	void testSetTurretRotation() {
-		fail("Not yet implemented");
+		tank.setTurretRotation(ogTurretRotation.x, ogTurretRotation.y, ogTurretRotation.z);
+		
+		assertEquals(0.0f, tank.getTurretRotation().x, 0.01f);
+		assertEquals(0.0f, tank.getTurretRotation().y, 0.01f);
+		assertEquals(0.0f, tank.getTurretRotation().z, 0.01f);
+		
+		tank.setTurretRotation(updatedTurretRotate.x, updatedTurretRotate.y, updatedTurretRotate.z);
+		
+		assertEquals(101.0f, tank.getTurretRotation().x, 0.01f);
+		assertEquals(3.0f, tank.getTurretRotation().y, 0.01f);
+		assertEquals(134.0f, tank.getTurretRotation().z, 0.01f);
+		
+		// SPECIAL TO SETTURRETROTATION
+		tank.setTurretRotation(11.0f, 7.0f, 14.0f);
+		
+		assertEquals(11.0f, tank.getTurretRotation().x, 0.01f);
+		assertEquals(7.0f, tank.getTurretRotation().y, 0.01f);
+		assertEquals(14.0f, tank.getTurretRotation().z, 0.01f);
 	}
 }
