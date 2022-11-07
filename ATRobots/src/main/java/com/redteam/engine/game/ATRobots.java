bdfe8b0f4a73fa8b7e1e76f018cd718bd2fa4b1a
Daniel Pellegrino <64383789+danpellegrino@users.noreply.github.com>
2022-11-07 13:15:24 -0500

    Updated .gitignore + added gameStates

--- ATRobots/src/main/java/com/redteam/engine/game/ATRobots.java
+++ ATRobots/src/main/java/com/redteam/engine/game/ATRobots.java
@@ -1,15 +1,21 @@
 package com.redteam.engine.game;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 import com.redteam.engine.core.Engine;
 import com.redteam.engine.core.Window;
 import com.redteam.engine.utils.Consts;
 import com.redteam.engine.core.entity.Robot;
 
 public class ATRobots {
-	private static Robot robotName = new Robot();
+	private static Robot robotName;
 	private static Window window;
+	private static Window mainMenuWindow;
+	private static Engine engine;
 	private static TestGame game;
-	
+	static BufferedReader reader = new BufferedReader(
+            new InputStreamReader(System.in));
 
 	// Game States
 	public enum State
@@ -17,15 +23,41 @@ public class ATRobots {
 	    START, RUNNING;
 	}
 	
+	// Current Game State
+	public static State state = State.START;
+	
 	// Main function
 	public static void main(String[] args) throws Exception {
-			if(robotName.compile()) {
-			window = new Window(Consts.TITLE, 1600, 900, true);
-			game = new TestGame();
-			Engine engine = new Engine();
-			engine.start();
-		}			
+		
+		run();
+				
 	}
+	
+	public static void run() throws Exception
+	{
+	    switch(state)
+	    {
+	    case START:
+	    	String name = reader.readLine();
+	    	if(name.trim().equals("run")) {
+		        state = State.RUNNING;
+		        run();
+	    	}
+	        break;
+	    case RUNNING:
+	    	robotName = new Robot();
+	    	if(robotName.compile()) {
+		    	window = new Window(Consts.TITLE, 1600, 900, true);
+				game = new TestGame();
+				Engine engine = new Engine();
+				engine.start();
+	    	}
+	        break;
+	    default:
+	        throw new RuntimeException("Unknown state: " + state);
+	    }
+	}
+
 		
 	public static Window getWindow() { 
 		return window;
--- ATRobots/src/main/java/com/redteam/engine/game/MainMenu.java
+++ ATRobots/src/main/java/com/redteam/engine/game/MainMenu.java
@@ -1,5 +1,38 @@
 package com.redteam.engine.game;
 
-public class MainMenu {
+import com.redteam.engine.core.ILogic;
+import com.redteam.engine.core.MouseInput;
+
+public class MainMenu implements ILogic {
+
+	@Override
+	public void init() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void input() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(float interval, MouseInput mouseInput) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cleanup() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
