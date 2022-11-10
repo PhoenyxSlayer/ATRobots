4bb4622721db44380c856e723fd8dc8858748018
Daniel Pellegrino <64383789+danpellegrino@users.noreply.github.com>
2022-11-09 19:02:06 -0500

    Instead of String Concatenation, now uses a StringBuilder w/ .append

--- ATRobots/src/main/java/com/redteam/engine/game/debug/gui/DebugCurrentEntitiesGUI.java
+++ ATRobots/src/main/java/com/redteam/engine/game/debug/gui/DebugCurrentEntitiesGUI.java
@@ -16,14 +16,14 @@ public class DebugCurrentEntitiesGUI implements iDebugGUI {
 
 	public void update() {
 		if(entities != null) {
-			String currentEntities = "";
+			StringBuilder currentEntities = new StringBuilder();
 			for (Entity ent : entities) {
-				currentEntities += "\nID: "+ ent.getID();
-				currentEntities += "\nPos:"
-								+	"\nX: " + ((int) (ent.getPos().x*100)) / 100.
-								+	"\nY: " + ((int) (ent.getPos().y*100)) / 100.
-								+	"\nZ: " + ((int) (ent.getPos().z*100)) / 100.
-								+	"\n";
+				currentEntities.append("\nID: ").append(ent.getID());
+				currentEntities.append("\nPos:" )
+							   .append("\nX: ").append(((int) (ent.getPos().x * 100)) / 100.)
+							   .append("\nY: ").append(((int) (ent.getPos().y * 100)) / 100.)
+							   .append("\nZ: ").append(((int) (ent.getPos().z * 100)) / 100.)
+							   .append("\n");
 			}
 			ImGui.text("Current Entities:\n " + currentEntities);
 		}
