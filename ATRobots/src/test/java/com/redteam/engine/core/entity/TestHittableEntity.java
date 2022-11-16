7bb53683433afdb895f405efd7b86fea914b5c39
Daniel Pellegrino <danpellegrino1@gmail.com>
2022-11-15 21:53:33 -0500

    Finished TestHittableEntity

--- ATRobots/src/test/java/com/redteam/engine/core/entity/TestHittableEntity.java
+++ ATRobots/src/test/java/com/redteam/engine/core/entity/TestHittableEntity.java
@@ -23,13 +23,15 @@ class TestHittableEntityClass extends HittableEntity {
 }
 
 class TestHittableEntity {
-    Vector3f originalPos = new Vector3f (0.0f,0.0f,0.0f);
-    Vector3f originalRot = new Vector3f (0.0f,0.0f,0.0f);
-
-    TestHittableEntityClass testEntity = new TestHittableEntityClass("tank", null, originalPos, originalRot, 1.0f, 5.0f);
-
+    Vector3f originalPos = new Vector3f(0.0f,0.0f,0.0f);
+    Vector3f originalRot = new Vector3f(0.0f,0.0f,0.0f);
     Vector3f updatedPos = new Vector3f(65.0f, 3.0f, 21.0f);
     Vector3f updatedRot = new Vector3f(44.0f, 72.0f, 10.0f);
+
+    TestHittableEntityClass testEntity = new TestHittableEntityClass("tank", null, originalPos, originalRot, 1.0f, 5.0f);
+    TestHittableEntityClass secondEntity = new TestHittableEntityClass("secondTank", null, updatedPos, updatedRot, 1.0f, 5.0f);
+
+    Vector3f[] testHitBox;
 
     // CONSTRUCTOR TEST
     @Test
@@ -40,14 +42,64 @@ class TestHittableEntity {
         assertEquals(1.0f, testEntity.getScale());
         assertEquals(5.0f, testEntity.getHitBoxScale());
 
-        TestHittableEntityClass secondEntity = new TestHittableEntityClass("secondTank", null, updatedPos, updatedRot, 2.0f, 6.0f);
-
         assertEquals("secondTank", secondEntity.getID());
         assertEquals(updatedPos, secondEntity.getPos());
         assertEquals(updatedRot, secondEntity.getRotation());
-        assertEquals(2.0f, secondEntity.getScale());
-        assertEquals(6.0f, secondEntity.getHitBoxScale());
+        assertEquals(1.0f, secondEntity.getScale());
+        assertEquals(5.0f, secondEntity.getHitBoxScale());
     }
 
+    // FORM CUBE TEST
+    @Test
+    void testFormCube() {
+        testHitBox = new Vector3f[]{
+                // front face
+                new Vector3f(-2.5f,2.5f,2.5f), // top left
+                new Vector3f(2.5f,2.5f,2.5f), // top right
+                new Vector3f(2.5f, -2.5f, 2.5f), // bottom right
+                new Vector3f(-2.5f, -2.5f, 2.5f), // bottom left
 
+                // back face
+                new Vector3f(-2.5f,2.5f,-2.5f), // top left
+                new Vector3f(2.5f,2.5f,-2.5f), // top right
+                new Vector3f(2.5f, -2.5f, -2.5f), // bottom right
+                new Vector3f(-2.5f, -2.5f, -2.5f) // bottom left
+        };
+
+        assertTrue(new Vector3f(-2.5f,2.5f,2.5f).equals(testHitBox[0]));
+        assertTrue(new Vector3f(2.5f,2.5f,2.5f).equals(testHitBox[1]));
+        assertFalse(new Vector3f(2.6f,-2.5f,2.5f).equals(testHitBox[2]));
+        assertTrue(new Vector3f(-2.5f,-2.5f,2.5f).equals(testHitBox[3]));
+
+        assertFalse(new Vector3f(-45.0f,2.5f,-2.5f).equals(testHitBox[4]));
+        assertFalse(new Vector3f(2.5f,10.0f,-2.5f).equals(testHitBox[5]));
+        assertTrue(new Vector3f(2.5f,-2.5f,-2.5f).equals(testHitBox[6]));
+        assertFalse(new Vector3f(-24.0f,-2.5f,9.0f).equals(testHitBox[7]));
+    }
+
+    // PASS THROUGH TEST
+    @Test
+    void testPassThrough() {
+        testEntity.setPos(0,0,0);
+
+        Vector3f testPos = new Vector3f(-2.5f, 1.2f, 0.7f);
+        assertTrue(testEntity.passThrough(testPos));
+
+        testPos = new Vector3f(-5.0f, 1.2f, 0.7f);
+        assertFalse(testEntity.passThrough(testPos));
+    }
+
+    // SECOND PASS THROUGH TEST
+    @Test
+    void testPassThrough2() {
+        testEntity.setPos(0,0,0);
+        testEntity.formCube();
+        secondEntity.setPos(-2.3f, 1.2f, 0.7f);
+        secondEntity.formCube();
+        assertTrue(testEntity.passThrough(secondEntity));
+
+        secondEntity.setPos(10.0f,2.0f,3.0f);
+        secondEntity.formCube();
+        assertFalse(testEntity.passThrough(secondEntity));
+    }
 }
