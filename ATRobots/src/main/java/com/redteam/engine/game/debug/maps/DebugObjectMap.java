45445658b999d78320656fbb2cf86c3bdd10c732
Daniel Pellegrino <64383789+danpellegrino@users.noreply.github.com>
2022-12-03 00:01:39 -0500

    Updated Outer Terrain to adjust to any size

--- ATRobots/src/main/java/com/redteam/engine/game/debug/maps/DebugObjectMap.java
+++ ATRobots/src/main/java/com/redteam/engine/game/debug/maps/DebugObjectMap.java
@@ -54,18 +54,18 @@ public class DebugObjectMap implements iObjMapping {
 
             Material borders = new Material(new Texture(DebugMode.getObjectLoader().loadTexture("textures/MineCraftGrass.jpg")), 0.1f);
       		
-        	addTerrain(new Terrain(new Vector3f(-75,0,-300), DebugMode.getObjectLoader(), borders));
-      		addTerrain(new Terrain(new Vector3f(-75,0,0), DebugMode.getObjectLoader(), borders));
+        	addTerrain(new Terrain(new Vector3f(-Constants.X_BORDER,0,-(Constants.Z_BORDER * 2)), DebugMode.getObjectLoader(), borders));
+      		addTerrain(new Terrain(new Vector3f(-Constants.X_BORDER,0,0), DebugMode.getObjectLoader(), borders));
 
       		//Right side of map
-      		addTerrain(new Terrain(new Vector3f(75,0,-150), DebugMode.getObjectLoader(), borders));
-      		addTerrain(new Terrain(new Vector3f(75,0,-300), DebugMode.getObjectLoader(), borders));
-      		addTerrain(new Terrain(new Vector3f(75,0,0), DebugMode.getObjectLoader(), borders));
+      		addTerrain(new Terrain(new Vector3f(Constants.X_BORDER,0,-Constants.Z_BORDER), DebugMode.getObjectLoader(), borders));
+      		addTerrain(new Terrain(new Vector3f(Constants.X_BORDER,0,-(Constants.Z_BORDER * 2)), DebugMode.getObjectLoader(), borders));
+      		addTerrain(new Terrain(new Vector3f(Constants.X_BORDER,0,0), DebugMode.getObjectLoader(), borders));
       		
       		//Left Side map
-      		addTerrain(new Terrain(new Vector3f(-225,0,-300), DebugMode.getObjectLoader(), borders));
-      		addTerrain(new Terrain(new Vector3f(-225,0,-150), DebugMode.getObjectLoader(), borders));
-      		addTerrain(new Terrain(new Vector3f(-225,0,0), DebugMode.getObjectLoader(), borders));
+      		addTerrain(new Terrain(new Vector3f(-(Constants.Z_BORDER * 1.5f),0,-(Constants.Z_BORDER * 2)), DebugMode.getObjectLoader(), borders));
+      		addTerrain(new Terrain(new Vector3f(-(Constants.Z_BORDER * 1.5f),0,-Constants.Z_BORDER), DebugMode.getObjectLoader(), borders));
+      		addTerrain(new Terrain(new Vector3f(-(Constants.Z_BORDER * 1.5f),0,0), DebugMode.getObjectLoader(), borders));
       //Outer Terrain^^*/
     }
 
